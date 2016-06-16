f80b2d1fee2a4722ca627e683cddee22060a76d9
Matt Heard <matt.heard@powershop.co.nz>
2016-06-16 20:21:12 +1200

    Click a button to take a snapshot

--- diary.js
+++ diary.js
@@ -1,16 +1,22 @@
 var deviceId = null;
+var video = document.getElementById("video");
+var button = document.getElementById("button");
+var canvas = document.getElementById("canvas");
+var img = document.getElementById("img");
+var width = 400;
+var height = 300;
 
 var initialiseVideoStream = function(stream) {
-  mediaStream = stream;
+  var mediaStream = stream;
 
-  var video = document.getElementById("previewVideo");
+  var video = document.getElementById("video");
   video.src = URL.createObjectURL(mediaStream);
 };
 
 var videoConstraints = {
   video : {
-    width: 1280,
-    height: 720,
+    width: width,
+    height: height,
     deviceId: deviceId
   }
 };
@@ -32,8 +38,40 @@ var devicesCallback = function(devices) {
   nextWebCam();
 };
 
+var setupCanvas = function() {
+  canvas.setAttribute("width", width);
+  canvas.setAttribute("height", height);
+};
+
+var takeAPicture = function() {
+  var context = canvas.getContext("2d");
+  context.drawImage(video, 0, 0, width, height);
+
+  var data = canvas.toDataURL("image/png");
+  img.setAttribute("src", data);
+};
+
+var addButtonListener = function() {
+  button.addEventListener("click", function(event) {
+    takeAPicture();
+    event.preventDefault();
+  }, false);
+};
+
+var clearImage = function() {
+  var context = canvas.getContext("2d");
+  context.fillStyle = "#AAA";
+  context.fillRect(0, 0, canvas.width, canvas.height);
+
+  var data = canvas.toDataURL("image/png");
+  img.setAttribute("src", data);
+};
+
 var init = function() {
+  setupCanvas();
+  clearImage();
   navigator.mediaDevices.enumerateDevices().then(devicesCallback);
+  addButtonListener();
 };
 
 init();
